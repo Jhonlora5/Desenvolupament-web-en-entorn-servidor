bd6d9bafaef1b74aa219bf58c9ea687217cc8739
Jhonlora5 <61328567+Jhonlora5@users.noreply.github.com>
2024-11-14 13:17:17 +0100

    Delete reCaptchaKeys directory

--- reCaptchaKeys/keys.autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Jonathan Lopez Ramos jaaaaaaarrrrrrr*/
-$_SESSION['key-client']="6LdM4nQqAAAAAHi_d_9xUdLLX1lx_aLwJP8UlTYS";
-$_SESSION['key-server']="6LdM4nQqAAAAACaBSmZS2fyjtlKs21Sa0RXPX8qq";
-?>
